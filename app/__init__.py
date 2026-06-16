e714d3475b3756b859c858af7704b76d323749c7
Jonna Al-Khanji <jonna.alkhanji@gmail.com>
2026-06-16 13:20:03 -0700

    Add database setup for users

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
+import os
 from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "smartcart.db"),
+    )
+
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    from . import db
+    db.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
