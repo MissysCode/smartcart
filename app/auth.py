897bd946b958966516d328a4fe671576e4d47aaa
Jonna Al-Khanji <jonna.alkhanji@gmail.com>
2026-06-16 13:01:00 -0700

    Add login and registration forms

--- app/auth.py
+++ app/auth.py
@@ -3,12 +3,12 @@ from flask import Blueprint, render_template
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/register")
+@auth.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
